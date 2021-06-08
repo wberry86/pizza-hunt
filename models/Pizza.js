f78c1e05a99b5101882d43e65f78cfb8e6b05270
William Berry <76190311+wberry86@users.noreply.github.com>
2021-06-08 16:46:38 -0500

    added controllers

---
+++ models/Pizza.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+const Pizza = model('Pizza', PizzaSchema);
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+
+module.exports = Pizza;
